072615fa42f9e212150be2f095430097b3a7d2df
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-01-30 13:01:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// Variables containing required packages
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-// Variables to connect modules to application
+
 const generateMarkdown = require("./utils/generateMarkdown");
 const licenseBadge = require("./utils/licenseBadge").licenseBadge;
 const questions = require("./utils/questions").questions;
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to initialize program and create README file
+// function to create README file
 async function init() {
   try {
     const answers = await inquirer.prompt(questions);
