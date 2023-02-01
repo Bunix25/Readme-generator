cf059570f65013163fc3cfaa8064dec60f157742
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-02-01 16:35:14 -0500

    Update licenseBadge.js

--- utils/licenseBadge.js
+++ utils/licenseBadge.js
@@ -1,4 +1,4 @@
-
+//licenseBadge
 function licenseBadge(value) {
   if (value === "GNU AGPLv3") {
     return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
